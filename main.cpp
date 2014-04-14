0363fd7d0b9087a29925bf87037c01efc96c7144
lord-surovtsev <lord.surovtsev@gmail.com>
2014-04-14 14:02:46 +0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#include <QtCore/QCoreApplication>
+
+#include <cuda_runtime.h>
+
+#include "code_cuda.cu"
+
+extern "C"
+cudaError_t cuda_main();
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //run your cuda application
+    cudaError_t cuerr = cuda_main();
+    //check for errors is always a good parctice!
+    if (cuerr != cudaSuccess)
+    {
+        cout << "CUDA Error: " << cudaGetErrorString( cuerr ) << endl;
+    }
+
+    return a.exec();
+}
